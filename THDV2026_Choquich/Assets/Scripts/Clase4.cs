5125e34070db8534cbd8b07082b12fc06a3ff5ab
Andres <achoquich@gmail.com>
2026-09-09 20:10:32 -0300

    Clase 5
    Uso de arrays para guardar pares

--- THDV2026_Choquich/Assets/Scripts/Clase4.cs
+++ THDV2026_Choquich/Assets/Scripts/Clase4.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -5,9 +6,11 @@ public class Clase4 : MonoBehaviour
 {
 
     float tiempo = 0;
-    private int segundos = 10;
+    private int segundos = 0;
     private bool terminado = false;
-    private int duracion = 0;
+    private int duracion = 10;
+    int[] segundospares = new int[5];
+    int posicionArray = 0;
 
     void Update()
     {
@@ -22,16 +25,56 @@ public class Clase4 : MonoBehaviour
 
             if (tiempo >= 1)
             {
-                segundos--;
-                Debug.Log("Segundo: " + segundos);
+                segundos++;
+
+                if (segundos % 2 == 0)
+                {
+                    Debug.Log("Segundo: " + segundos + " - PAR");
+
+                    segundospares[posicionArray] = segundos;
+                    posicionArray++;
+
+                }
+                else
+                {
+                    Debug.Log("Segundo: " + segundos);
+                }    
+                
                 tiempo = 0;
 
-                if (segundos <= duracion)
+                if (segundos >= duracion)
                 {
                     terminado = true;
                     Debug.Log("Timer Terminado");
+
+                    MostrarPares();
                 }
+
+               
+
             }
         }
     }
+    void MostrarPares()
+    {
+        for (int i = 0; i < segundospares.Length; i++)
+        {
+            Debug.Log("Par: " + segundospares[i]);
+        }
+    }
+
+    //void MostrarParesMayores()
+    //{
+        
+
+        //while (MostrarPares())
+        //{
+           
+            
+            //Debug.Log("Par: " + segundospares[i]);
+        //}
+    //}
+
 }
+
+//a
